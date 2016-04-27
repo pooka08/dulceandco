a17dbd71888ca917f033a3a140a3d1459a8958bf
Karen Kho <kkho@exist.com>
2016-04-27 11:37:11 +0800

    contact us js

--- js/contact_me.js
+++ js/contact_me.js
@@ -50,7 +50,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>sherwin@dulceandco.com ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Please email me directly to sherwin@dulceandco.com");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
